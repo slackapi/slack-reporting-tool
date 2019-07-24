0ab12f944f2833b8d5b3207444e0f1e04dfb549a
Jim Ray <jray@slack-corp.com>
2019-07-24 12:38:41 -0400

    Fixed up start scripts

---
+++ tools/ngrok.js
@@ -0,0 +1,25 @@
+let ngrok;
+try{
+  ngrok = require('ngrok');
+} catch (error){
+  console.error('ngrok npm package not found. Run `npm install --save-optional`');
+}
+
+const authtoken = process.env.NGROK_AUTHTOKEN;
+const subdomain = process.env.NGROK_SUBDOMAIN;
+
+//TODO: make this work without auth or a subdomain?
+if (!authtoken && subdomain) {
+  console.error('ngrok needs an auth token and subdomain');
+  return;
+}
+
+(async () => {
+  const url = await ngrok.connect({
+    subdomain,
+    authtoken,
+    addr: process.env.PORT | '3000'
+  });
+
+  console.log(`🚚 ngrok: App is at ${url} \n          Use http://127.0.0.1:4040 to inspect`)
+})();

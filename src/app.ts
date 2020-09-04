df16037a81f15affc6237c401959d0b6b3aaa6b7
Colm Doyle <cdoyle@slack-corp.com>
2020-09-04 17:26:15 +0100

    Handle view submission

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ require('dotenv').config();
 import { App, LogLevel } from '@slack/bolt';
 import { createConnection, RowDataPacket } from "mysql2/promise";
 const responses = require('./bot_responses');
-const modal = require('./modal');
+import modal from './modal';
 
 /* CONFIG */
 // The triage channel for admins to use.
@@ -16,7 +16,7 @@ const databaseConfig = {
   password: process.env.DB_PASSWORD
 }
 
-const botToken : string = process.env.SLACK_BOT_TOKEN as string;
+const botToken: string = process.env.SLACK_BOT_TOKEN as string;
 
 function getApp(): App {
   if (botToken && botToken.startsWith('xoxb-')) {
@@ -89,7 +89,7 @@ const app = getApp();
  * Respond with a modal asking for an optional bit of context from the reporting user
  * plus the option to report the message anonymously
  */
-app.shortcut({ callback_id: 'report_message' }, async ({ body, ack, context }) => {
+app.shortcut({ callback_id: 'report_message', type: 'message_action' }, async ({ body, ack, context }) => {
   // acknowledge immediately
   await ack();
 
@@ -97,46 +97,45 @@ app.shortcut({ callback_id: 'report_message' }, async ({ body, ack, context }) =
   const result = await app.client.views.open({
     token: context.botToken,
     trigger_id: body.trigger_id,
-    view: modal.confirm
+    view: modal.confirm(body.message_ts, body.channel.id, body.message.text, body.message.user)
   });
 
-  //Save the initial conversation ID and the viewID for later use
-  //const reported_conversation = result.view.blocks[0].element.private_metadata;
-  //const view_id = result.view.id;
+});
+
 
 /*
  * Handle the actual submission of the reported message once the user confirms via the modal
- * TODO: https://slack.dev/bolt-js/concepts#view_submissions
 */
-app.action({ callback_id: 'report_confirm' }, async ({ body, ack, context }) => {
+app.view('report_confirm', async ({ body, ack, context, view }) => {
   // acknowledge receipt of dialog
   await ack();
 
   try {
     // the form data the user submitted: optional comment and whether to report anonymously
-    const reported_context = body.submission;
+    const reported_context = view.state.values;
+    console.log(JSON.stringify(view, null, 2));
     let report_header = '';
     // who reported the message, handle anonymity
-    const report_anonymously = (reported_context.anonymous === 'true');
+    const report_anonymously = (reported_context['anonymous_report_selector']['anonymous_report_selector']['selected_option']['value'] === 'true');
     const reporter_id = body.user.id;
     const reporter = report_anonymously ? `Someone anonymously` : `<@${body.user.id}>`;
     // addtional commentary, handle none
-    const report_comment = reported_context.comment;
+    const report_comment = reported_context['report_comment']['report_comment']['value'];
     if (report_comment !== null) {
       report_header = `${reporter} reported the following message with the comment: ${report_comment}`
     } else {
       report_header = `${reporter} reported the following message.`
     }
     // the message being reported, stored in the state field of the dialog
-    const reported_message = JSON.parse(`${body.state}`);
+    const reported_message = JSON.parse(`${view.private_metadata}`);
 
-    const message_ts = reported_message.message.ts.replace('.', '');
+    const message_ts = reported_message.message_ts.replace('.', '');
     // TODO: this should use https://api.slack.com/methods/chat.getPermalink instead
-    const message_link = `https://${body.team.domain}.slack.com/archives/${body.channel.id}/p${message_ts}`;
+    const message_link = `https://${body.team.domain}.slack.com/archives/${reported_message.channel_id}/p${message_ts}`;
 
     try {
       // Call the chat.postMessage method with the bot token, post reported message to the triage channel
-      const result = await app.client.chat.postMessage({
+      await app.client.chat.postMessage({
         // The token you used to initialize your app is stored in the `context` object
         token: context.botToken,
         channel: triage_channel,
@@ -156,7 +155,7 @@ app.action({ callback_id: 'report_confirm' }, async ({ body, ack, context }) => 
             "type": "section",
             "text": {
               "type": "mrkdwn",
-              "text": `*<@${reported_message.message.user}> said*:\n${reported_message.message.text}`
+              "text": `*<@${reported_message.message_author}> said*:\n${reported_message.message_text}`
             }
           },
           {
@@ -178,11 +177,15 @@ app.action({ callback_id: 'report_confirm' }, async ({ body, ack, context }) => 
         console.log("DM the reporting user");
         console.log(reporter_id);
         // Post a DM to the user who reported the message confirming report
-        const confirmation = await app.client.chat.postMessage({
+        const confirmation_text = report_anonymously ? 
+          "Thank you for reporting a message, we appreciate your help in keeping this workspace respectful and inclusive.\nThe workspace administrators will review the message and determine the appropriate response, but your identity will not be shared." : 
+          "Thank you for reporting a message, we appreciate your help in keeping this workspace respectful and inclusive.\nThe workspace administrators will review the message and determine the appropriate response.";
+        
+          await app.client.chat.postMessage({
           token: context.botToken,
           channel: reporter_id,
           as_user: true,
-          text: "Thank you for reporting a message, we appreciate your help in keeping this workspace respectful and inclusive.\nThe workspace administrators will review the message and determine the appropriate response."
+          text: confirmation_text
         });
       }
       catch (error) {
@@ -208,7 +211,7 @@ app.event('app_home_opened', async ({ event, context, say }) => {
     count: 1
   })
 
-  if (!history.messages.length) {
+  if (!(history.messages as any).length) {
     await say(responses.welcome);
   }
 });
---
+++ src/modal.ts
@@ -0,0 +1,82 @@
+import { View } from '@slack/types';
+
+export default {
+  confirm(message_ts: string, channel_id: string, message_text: string, message_author: string): View {
+    return {
+      type: 'modal',
+      callback_id: 'report_confirm',
+      title: {
+        type: 'plain_text',
+        text: 'My App',
+        emoji: true,
+      },
+      submit: {
+        type: 'plain_text',
+        text: 'Report',
+        emoji: true,
+      },
+      close: {
+        type: 'plain_text',
+        text: 'Cancel',
+        emoji: true,
+      },
+      private_metadata: `${JSON.stringify({message_ts, channel_id, message_text, message_author})}`,
+      blocks: [
+        {
+          type: 'input',
+          element: {
+            action_id: 'report_comment',
+            type: 'plain_text_input',
+            multiline: true,
+          },
+          label: {
+            type: 'plain_text',
+            text: 'Why are you reporting this message?',
+            emoji: true,
+          },
+          block_id: 'report_comment'
+        },
+        {
+          type: 'context',
+          elements: [
+            {
+              type: 'mrkdwn',
+              text: 'Optionally add any context or information you believe is important. This will be shared with moderators along with the original message.',
+            },
+          ],
+        },
+        {
+          type: 'input',
+          block_id: 'anonymous_report_selector',
+          element: {
+            action_id: 'anonymous_report_selector',
+            type: 'static_select',
+            options: [
+              {
+                text: {
+                  type: 'plain_text',
+                  text: 'Yes, include my name with the report',
+                  emoji: true,
+                },
+                value: 'false',
+              },
+              {
+                text: {
+                  type: 'plain_text',
+                  text: 'No, report anonymously',
+                  emoji: true,
+                },
+                value: 'true',
+              },
+            ],
+          },
+          label: {
+            type: 'plain_text',
+            text: 'Include your name?',
+            emoji: true,
+          },
+        },
+      ],
+    }
+  }
+}
